Added different normalization options. Fixed verbose in sparse coding obj. Changed init scheme in sparse coding

data/seismic_hdf5.py
@@ -5,7 +5,7 @@ import random
 import TFSparseCode.data.utils as utils
 
 class SeismicDataHdf5(object):
-    def __init__(self, filename, example_size=None, seed=None):
+    def __init__(self, filename, example_size=None, seed=None, normalize=True):
         if seed is not None:
             random.seed(seed)
 
@@ -16,7 +16,14 @@ class SeismicDataHdf5(object):
         (self.num_samples, self.num_channels, self.num_stations, self.num_events) = self.data.shape
         self.data = np.transpose(self.data, [3, 0, 2, 1])
         self.num_features = self.num_channels * self.num_stations
+        #Data is now [num_events, num_samples, num_stations * num_channels]
         self.data = np.reshape(self.data, [self.num_events, self.num_samples, self.num_features])
+
+        #Normalize by entire dataset, ind by feature
+        if(normalize):
+            mean = np.mean(self.data, axis=(0, 1), keepdims = True)
+            std = np.std(self.data, axis=(0, 1), keepdims = True)
+            self.data = (self.data - mean)/std
 
         self.station_group = np.reshape(np.array(range(self.num_features)), [self.num_stations, self.num_channels])
         self.station_title = ["station_" + str(g[0]) for g in self.station_group]
@@ -27,7 +34,7 @@ class SeismicDataHdf5(object):
 
     def loadStream(self, filename, verbose=True):
         self.file = h5py.File(filename, 'r')
-        #Using only first 5 for now
+        #Using only first 5 for now, TODO param
         data = []
         max_idx = 5
         for idx in range(max_idx):

models/lcaDeepSC.py
@@ -115,8 +115,8 @@ class lcaDeepSC(object):
                     low_init_val = -.1
                     high_init_val = .1
                 else:
-                    low_init_val = 0
-                    high_init_val  = 1.01*curr_l1_weight
+                    low_init_val = .8*curr_l1_weight
+                    high_init_val  = 1.1*curr_l1_weight
                 potential_init = tf.random_uniform(act_shape, low_init_val, high_init_val, dtype=tf.float32)
                 self.reset_potential.append(curr_potential.assign(potential_init))
 
@@ -306,30 +306,49 @@ class lcaDeepSC(object):
     def step(self, sess, feed_dict, verbose, step, verbose_period=10):
         if(verbose):
             if((step+1) % verbose_period == 0):
+                #TODO store this to disk
                 #act_norm= [np.mean(a) for a in act_norm]
-                [recon_err, output_norm, act_max, nnz] = sess.run(
-                        [self.model["recon_error"], self.model["output_norm"], self.model["act_max"], self.model["nnz"]], feed_dict=feed_dict)
-
-                output_norm = [np.mean(a) for a in output_norm]
-                #pot_std = [np.mean(a) for a in pot_std]
+                stats_list = ["recon_error", "nnz", "output_norm", "input_norm"]
+                stats_nodes = [self.model[stat] for stat in stats_list]
+                output = sess.run(stats_nodes, feed_dict=feed_dict)
+                #[recon_err, output_norm, act_max, nnz] = sess.run(
+                #        [self.model["recon_error"], self.model["output_norm"], self.model["act_max"], self.model["nnz"]], feed_dict=feed_dict)
+                #pdb.set_trace()
 
                 outstr = ""
-                outstr += "%3d"%(step+1) + ": \trecon_error ["
-                for num in recon_err:
-                    if(num is None):
-                        outstr += "  None "
-                    else:
-                        outstr += "%7.2f"%num + ", "
-                outstr += "] \tnnz ["
-                for num in nnz:
-                    outstr += "%4.4f"%num + ", "
-                outstr += "] \toutput_norm["
-                for num in output_norm:
-                    outstr += "%7.5f"%num + ", "
-                outstr += "] \tact_max["
-                for num in act_max:
-                    outstr += "%7.5f"%num + ", "
-                outstr += "]"
+                outstr += "%4d"%(step+1) + ":"
+                for name, out in zip(stats_list, output):
+                    ndims = len(np.array(out).shape)
+                    if(ndims > 1):
+                        out = np.mean(out, axis=tuple(range(1, ndims)))
+                    outstr += " \t" + name + "["
+                    for layer_out in out:
+                        if(layer_out is None):
+                            outstr+= " None, "
+                        else:
+                            try:
+                                outstr += "%7.5f"%layer_out + ", "
+                            except:
+                                pdb.set_trace()
+                    outstr += "]"
+
+                #outstr = ""
+                #outstr += "%3d"%(step+1) + ": \trecon_error ["
+                #for num in recon_err:
+                #    if(num is None):
+                #        outstr += "  None "
+                #    else:
+                #        outstr += "%7.2f"%num + ", "
+                #outstr += "] \tnnz ["
+                #for num in nnz:
+                #    outstr += "%4.4f"%num + ", "
+                #outstr += "] \toutput_norm["
+                #for num in output_norm:
+                #    outstr += "%7.5f"%num + ", "
+                #outstr += "] \tact_max["
+                #for num in act_max:
+                #    outstr += "%7.5f"%num + ", "
+                #outstr += "]"
 
                 #print("%3f"%step, ": \trecon_error", recon_err, "\tl1_sparsity", l1_sparsity, "\tloss", loss, "\tnnz", nnz)
                 print(outstr)

models/sparseCode.py
@@ -11,25 +11,33 @@ class sparseCode(base):
         with tf.device(self.params.device):
             with tf.name_scope("placeholders"):
                 #TODO Split input into input and ground truth, since last input is never being used
+                curr_input_shape = [self.params.batch_size, ] + self.params.input_shape
                 self.input = tf.placeholder(tf.float32,
-                        shape=[self.params.batch_size, ] + self.params.input_shape,
+                        shape=curr_input_shape,
                         name = "input")
 
-                self.ndims_input = len(self.params.input_shape)
+                self.ndims_input = len(curr_input_shape)
                 #TODO add in for images instead of only 1d
-                if(self.ndims_input == 2):
+                if(self.ndims_input == 3):
                     (example_size, num_features) = self.params.input_shape
                 else:
                     print("Not implemented")
                     assert(0)
 
-
-                (data_mean, data_var) = tf.nn.moments(self.input, axes=1, keep_dims=True)
+                if(self.params.norm_ind_features):
+                    norm_reduction_idx = [1,]
+                else:
+                    norm_reduction_idx = [1,2]
+                (data_mean, data_var) = tf.nn.moments(self.input, axes=norm_reduction_idx, keep_dims=True)
                 if(self.params.norm_input):
                     calc_norm = ((self.input - data_mean)/tf.sqrt(data_var)) * self.params.target_norm_std
                     #Expand data_mean and data_var to have same shape as input
-                    data_mean = tf.tile(data_mean, [1, example_size, 1])
-                    data_var = tf.tile(data_var, [1, example_size, 1])
+                    if(self.params.norm_ind_features):
+                        data_mean = tf.tile(data_mean, [1, example_size, 1])
+                        data_var = tf.tile(data_var, [1, example_size, 1])
+                    else:
+                        data_mean = tf.tile(data_mean, [1, example_size, num_features])
+                        data_var = tf.tile(data_var, [1, example_size, num_features])
 
                     self.norm_input = tf.where(tf.equal(data_var, 0), data_mean, calc_norm)
                 else:
@@ -140,16 +148,15 @@ class sparseCode(base):
         np_recon = self.sess.run(self.input_recon, feed_dict=feed_dict)
 
         np_unscaled_input = feed_dict[self.input]
-        if(self.params.norm_input):
-            np_unscaled_recon = self.sess.run(self.unscaled_recon, feed_dict=feed_dict)
+        np_unscaled_recon = self.sess.run(self.unscaled_recon, feed_dict=feed_dict)
 
-        if(self.ndims_input == 2):
+        if(self.ndims_input == 3):
             plotRecon.plotRecon1D(np_recon, np_input, fn_prefix+"recon",
                     num_plot = self.params.num_plot_recon,
                     unscaled_img_matrix = np_unscaled_input, unscaled_recon_matrix=np_unscaled_recon,
                     groups=self.params.plot_groups, group_title=self.params.plot_group_title,
                     legend=self.params.legend)
-        elif(self.ndims_input == 2):
+        else:
             assert(0)
 
     def plotWeights(self, fn_prefix):